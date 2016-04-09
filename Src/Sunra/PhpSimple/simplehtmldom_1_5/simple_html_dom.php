4457414da13fc82c598141ce0494c3f3747fb168
mryanb <mryanb@users.noreply.github.com>
2016-04-09 08:32:48 -0500

    Update simple_html_dom.php
    Check if MAX_FILE_SIZE is already defined

--- Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
+++ Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
@@ -62,7 +62,10 @@ define('HDOM_INFO_ENDSPACE',7);
 define('DEFAULT_TARGET_CHARSET', 'UTF-8');
 define('DEFAULT_BR_TEXT', "\r\n");
 define('DEFAULT_SPAN_TEXT', " ");
-define('MAX_FILE_SIZE', 600000);
+if (!defined('MAX_FILE_SIZE'))
+{
+    define('MAX_FILE_SIZE', 600000);
+}
 // helper functions
 // -----------------------------------------------------------------------------
 // get html dom from file
