ec710a0084d4076b2e3006d307448fabbba2d827
sunra <sunra@yandex.ru>
2016-12-17 13:17:23 +0200

    issues/37
    old php version std value corrected

--- Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
+++ Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
@@ -73,7 +73,7 @@ if (!defined('MAX_FILE_SIZE'))
 // -----------------------------------------------------------------------------
 // get html dom from file
 // $maxlen is defined in the code as PHP_STREAM_COPY_ALL which is defined as -1.
-function file_get_html($url, $use_include_path = false, $context=null, $offset = -1, $maxLen=-1, $lowercase = true, $forceTagsClosed=true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
+function file_get_html($url, $use_include_path = false, $context=null, $offset=0, $maxLen=-1, $lowercase = true, $forceTagsClosed=true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
 {
     // We DO force the tags to be terminated.
     $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
