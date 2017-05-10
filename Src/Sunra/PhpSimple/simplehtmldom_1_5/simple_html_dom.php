55b70173e8694c8f28904a7516ae1cbeb4211f13
Zinta <>
2017-05-10 21:07:44 +0200

    fixing Call to undefined function mb_detect_encoding() error
    this happens with minimal php installation ( missing php-mbstring
    library)

--- Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
+++ Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
@@ -1245,7 +1245,7 @@ class simple_html_dom
         if (empty($charset))
         {
             // Have php try to detect the encoding from the text given to us.
-            $charset = mb_detect_encoding($this->root->plaintext . "ascii", $encoding_list = array( "UTF-8", "CP1252" ) );
+            $charset = (function_exists('mb_detect_encoding')) ? mb_detect_encoding($this->root->plaintext . "ascii", $encoding_list = array( "UTF-8", "CP1252" ) ) : false;
             if (is_object($debugObject)) {$debugObject->debugLog(2, 'mb_detect found: ' . $charset);}
 
             // and if this doesn't work...  then we need to just wrongheadedly assume it's UTF-8 so that we can move on - cause this will usually give us most of what we need...
